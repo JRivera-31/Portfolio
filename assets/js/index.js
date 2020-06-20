979f43a41c50400b4f22452979f275dc8a1ff5b5
Josh R <jriver35@uncc.edu>
2020-06-19 21:13:44 -0400

    Added resume pdf link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-const repoName = $("#reponame")
-const linkList = $("#links")
 const main = $("#main")
 
 $.ajax({
@@ -10,12 +8,6 @@ $.ajax({
     
     for (let i = 0; i < results.length; i++) {
         if (results[i].stargazers_count != 0) {
-            // repoName.text(results[i].name)
-            
-            // let article = "<li><a href=" + results[i].html_url + " target=_blank class=button>Github Repo</a></li>"
-            // let deployed = "<li><a href=" + results[i].homepage + " target=_blank class=button>Deployed App</a></li>"
-            // linkList.append(repo, deployed)
-
             let article = "<article><header><h2>" + results[i].name + "</h2></header><p>" + results[i].description + "</p>" +
                 "<ul class=actions special><li><a href=" + results[i].html_url + " target=_blank class=button>Github Repo</a></li><li><a href=" + results[i].homepage + " target=_blank class=button>Deployed App</a></li></ul></article>"
             
